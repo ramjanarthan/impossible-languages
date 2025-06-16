47ec54db561cb1fe7b733d448887d220dc27b2b0
Ram Janarthan <ram.janarthan11@gmail.com>
2025-06-16 17:47:27 +0100

    Update dataset generation in v2 to be base BLiMP with updated output path

--- README.md
+++ README.md
@@ -16,9 +16,13 @@ pip install torch
 
 ## Generating data:
 
-The data generation scripts are in the `data_generation/generation_projects/impossible_blimp` directory. The output will be in the `data_generation/outputs/impossible_blimp` directory.
+The data generation scripts are in the `data_generation/generation_projects/impossible_blimp` directory. The output will be in the `data_generation/outputs/impossible_blimp` directory. 
 
-To generate data, run the following command in the root project directory:
+The generation is split into two phases: generating a base dataset, and generating the impossible version of the dataset. For the first part, I adopt the BLiMP generation method, and for the second part, I apply a perturbation function to the base dataset.
+
+### Generating base dataset
+
+To generate the base dataset, run the following command in the root project directory:
 
 ```bash
 python -m data_generation.generation_projects.impossible_blimp.v2.<generator name>
@@ -28,6 +32,13 @@ For example:
 ```bash
 python -m data_generation.generation_projects.impossible_blimp.v2.distractor_agreement_rc
 ```
+
+### Generating impossible dataset
+
+The impossible datasets are modified versions of the base dataset, where the perturbation function is applied to the base dataset.
+
+To generate the impossible dataset, run the following command in the root project directory:
+
 
 ## Running experiments:
 
--- data_generation/generation_projects/impossible_blimp/v2/distractor_agreement_rc.py
+++ data_generation/generation_projects/impossible_blimp/v2/distractor_agreement_rc.py
@@ -3,16 +3,13 @@ from data_generation.utils.constituent_building import *
 from data_generation.utils.conjugate import *
 from data_generation.utils.randomize import choice
 from data_generation.utils.vocab_sets_dynamic import *
-from data_generation.utils.impossible_utils import PERTURBATIONS
+import datetime
 
-class ImpossibleDistractorAgreementRCGenerator(data_generator.BenchmarkGenerator):
-    PERTURBATION_KEYS_FOR_DATA_GENERATION = ["shuffle_nondeterministic", "shuffle_deterministic21", "shuffle_local3", "shuffle_local5", "shuffle_even_odd", "reverse_control", "reverse_partial", "reverse_full"]
-    PERTURBATION_KEYS_FOR_EVALUATION = ["english"] + PERTURBATION_KEYS_FOR_DATA_GENERATION
-
+class AgreementGenerator(data_generator.BenchmarkGenerator):
     def __init__(self):
         super().__init__(field="morphology",
                          linguistics="subject_verb_agreement",
-                         uid="impossible_distractor_agreement_relative_clause",
+                         uid="distractor_agreement_relative_clause",
                          simple_lm_method=True,
                          one_prefix_method=True,
                          two_prefix_method=False,
@@ -20,6 +17,12 @@ class ImpossibleDistractorAgreementRCGenerator(data_generator.BenchmarkGenerator
         self.all_reg_nouns = get_all_conjunctive([("category", "N"), ("irrpl", "")], get_all_common_nouns())
         self.safe_emb_verbs = get_all_transitive_verbs()
         self.safe_mat_verbs = np.setdiff1d(get_all_verbs(), get_all("past", "1"))
+
+    def make_metadata_dict(self):
+        metadata = {
+            "UID": self.uid,
+        }
+        return metadata
 
     def sample(self):
         # The cat   that was      eating the mice is        sleeping
@@ -58,25 +61,11 @@ class ImpossibleDistractorAgreementRCGenerator(data_generator.BenchmarkGenerator
         V_mat_args = verb_args_from_verb(V_mat_agree, subj=subj, aux=Aux_agree)
 
         data = {
-            "sentence_good_english": "%s %s %s %s %s %s %s %s." % (subj[0], rel[0], Aux_emb[0], V_emb[0], obj_emb[0], Aux_agree, V_mat_agree[0], join_args(V_mat_args["args"])),
-            "sentence_bad_english": "%s %s %s %s %s %s %s %s." % (subj[0], rel[0], Aux_emb[0], V_emb[0], obj_emb[0], Aux_not_agree, V_mat_not_agree[0], join_args(V_mat_args["args"])),
+            "sentence_good": "%s %s %s %s %s %s %s %s." % (subj[0], rel[0], Aux_emb[0], V_emb[0], obj_emb[0], Aux_agree, V_mat_agree[0], join_args(V_mat_args["args"])),
+            "sentence_bad": "%s %s %s %s %s %s %s %s." % (subj[0], rel[0], Aux_emb[0], V_emb[0], obj_emb[0], Aux_not_agree, V_mat_not_agree[0], join_args(V_mat_args["args"])),
         }
+        return data, data["sentence_good"]
 
-        for perturbation_key in ImpossibleDistractorAgreementRCGenerator.PERTURBATION_KEYS_FOR_DATA_GENERATION:
-            perturbation = PERTURBATIONS[perturbation_key]
-
-            # Impossible sentences
-            impossible_sentence_good = perturbation["perturbation_function"](data["sentence_good_english"])
-            impossible_sentence_bad = perturbation["perturbation_function"](data["sentence_bad_english"])
-
-            impossible_sentence_good = "".join(map(lambda x: perturbation["gpt2_tokenizer"].decode(x), impossible_sentence_good))
-            impossible_sentence_bad = "".join(map(lambda x: perturbation["gpt2_tokenizer"].decode(x), impossible_sentence_bad))
-
-            data[f"sentence_good_{perturbation_key}"] = impossible_sentence_good
-            data[f"sentence_bad_{perturbation_key}"] = impossible_sentence_bad
-
-        return data, data["sentence_good_english"]
-
-if __name__ == "__main__":
-    generator = ImpossibleDistractorAgreementRCGenerator()
-    generator.generate_paradigm(number_to_generate=10, rel_output_path="outputs/impossible_blimp/v2/%s.jsonl" % generator.uid)
+generator = AgreementGenerator()
+timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+generator.generate_paradigm(number_to_generate=1000, rel_output_path="outputs/impossible_blimp/v2/%s_%s.jsonl" % (generator.uid, timestamp))
