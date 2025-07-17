00a1e5cd92a71f31dd7cff45abc408b705a29e92
Ram Janarthan <ram.janarthan11@gmail.com>
2025-07-17 14:57:06 +0100

    Update base example in dependency parse analysis

--- analysis/dependency_parse.py
+++ analysis/dependency_parse.py
@@ -5,10 +5,14 @@ from spacy import displacy
 nlp = spacy.load('en_core_web_sm')
 
 # Example sentence to parse
-sentence = "Apple's CEO Tim Cook visited the company's headquarters in Cupertino."
+sentence = "Timothy didn't boast about himself."
+reverse_sentence = ". himself about🅁 boast't didnothyTim"
+reverse_parial_sentence = "Timothy didn't boast🅁. himself about"
+shuffle_det_sentence = "aboutTim boastothy himself't. didn"
+shuffle_non_det_sentence = ". himself didn about boastTimothy't"
 
 # Parse the sentence using spaCy
 doc = nlp(sentence)
 
 # Visualize the dependency tree with default settings
-displacy.serve(doc, style='dep', port=5001)
+displacy.serve(doc, style='dep', port=5004)
