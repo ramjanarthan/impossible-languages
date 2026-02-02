787e7fe4ed15dbbe4a7d72403c5de7b66db5625d
Ram Janarthan <ram.janarthan11@gmail.com>
2026-02-02 19:07:32 +0530

    Update generate.py to use token based undo operations

--- data_generation/generation_projects/impossible_generations/generate.py
+++ data_generation/generation_projects/impossible_generations/generate.py
@@ -1,15 +1,15 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import torch
-from data_generation.utils.impossible_utils import PERTURBATION_TO_HF_MODEL_NAME, VALID_UNDO_PERTURBATION_KEYS, UNDO_PERTURBATIONS
+from data_generation.utils.impossible_utils import PERTURBATION_TO_HF_MODEL_NAME, VALID_UNDO_PERTURBATION_KEYS, UNDO_PERTURBATIONS, PERTURBATIONS
 
 NUM_LINES = 10
 RAW_OUTPUT_DIR = "data_generation/outputs/impossible_generations/raw/"
 CORRECTED_OUTPUT_DIR = "data_generation/outputs/impossible_generations/corrected/"
 
-for perturbation in [ "shuffle_even_odd"]:
+for perturbation in [ "reverse_full", "reverse_partial", "shuffle_local3", "shuffle_local5", "shuffle_local10"]:
     model_id = PERTURBATION_TO_HF_MODEL_NAME[perturbation]
     model = GPT2LMHeadModel.from_pretrained(model_id)
-    tokenizer = GPT2Tokenizer.from_pretrained(model_id)
+    tokenizer = PERTURBATIONS[perturbation]['gpt2_tokenizer']   
 
     # Start with just the BOS token (no initial prompt)
     input_ids = torch.tensor([[tokenizer.bos_token_id]])
@@ -19,11 +19,13 @@ for perturbation in [ "shuffle_even_odd"]:
 
     for i in range(NUM_LINES):
         output = model.generate(input_ids, max_new_tokens=50, pad_token_id=tokenizer.eos_token_id, do_sample=True)
+
         generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
         raw_outputs.append(generated_text)
 
-        corrected_output = UNDO_PERTURBATIONS[perturbation]['perturbation_function'](generated_text)
-        corrected_text = tokenizer.decode(corrected_output, skip_special_tokens=True)
+        undo_input =  UNDO_PERTURBATIONS[perturbation]['gpt2_tokenizer'].encode(generated_text) #output[0].tolist()
+        corrected_output = UNDO_PERTURBATIONS[perturbation]['perturbation_function'](undo_input)
+        corrected_text = UNDO_PERTURBATIONS[perturbation]['gpt2_tokenizer'].decode(corrected_output, skip_special_tokens=True)
         corrected_outputs.append(corrected_text)
 
     # Save generated text to file
@@ -33,5 +35,4 @@ for perturbation in [ "shuffle_even_odd"]:
     with open(CORRECTED_OUTPUT_DIR + f"{perturbation}.txt", "w+") as f:
         f.write("\n".join(corrected_outputs))
     
-
     print(f"Generated {NUM_LINES} sentences for {perturbation}.")
