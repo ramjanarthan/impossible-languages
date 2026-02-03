ea07d4d137168c461a5bc8a6962617af76394fc1
Ram Janarthan <ram.janarthan11@gmail.com>
2026-02-03 16:51:12 +0530

    Try calculating probabilities based on next_token_logits

--- test.py
+++ test.py
@@ -1,15 +1,20 @@
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
+import torch
+import torch.nn.functional as F
 
 model_id = "mission-impossible-lms/no-shuffle-gpt2"
 device = "mps"
 model, tokenizer = GPT2LMHeadModel.from_pretrained(model_id).to(device), GPT2Tokenizer.from_pretrained(model_id)
 
-test_prompt = "He is a very "
+empty_prompt = "Hi " #tokenizer.bos_token
 
-input_ids = tokenizer.encode(test_prompt, return_tensors='pt').to(device)
+test_sentence = "He is a boy"
+test_sentence_tokens = tokenizer.encode(test_sentence)
 
-output = model.generate(input_ids, max_new_tokens=50, pad_token_id=tokenizer.eos_token_id, do_sample=True)
+input_ids = tokenizer.encode(empty_prompt, return_tensors='pt').to(device)
+output = model(input_ids)
 
-decoded_output = tokenizer.decode(output[0], skip_special_tokens=True)
+next_token_logits = output.logits[:, -1, :]
+probabilities = F.log_softmax(next_token_logits, dim=0)
 
-print("Decoded output - ", decoded_output)
+print(torch.sum(probabilities))
