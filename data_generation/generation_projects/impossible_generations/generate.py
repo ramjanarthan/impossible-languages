631fdabc5a44ac6a58d0145593847120c065d195
Ram Janarthan <ram.janarthan11@gmail.com>
2026-02-09 16:37:56 +0530

    Update data generation to store text generations as json objects. Include external model generations

--- data_generation/generation_projects/impossible_generations/generate.py
+++ data_generation/generation_projects/impossible_generations/generate.py
@@ -1,9 +1,10 @@
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, AutoTokenizer, AutoModelForCausalLM
 import torch
 from tqdm import tqdm
 from data_generation.utils.impossible_utils import PERTURBATION_TO_HF_MODEL_NAME, VALID_UNDO_PERTURBATION_KEYS, UNDO_PERTURBATIONS, PERTURBATIONS
+import json
 
-NUM_LINES = 50
+NUM_LINES = 5
 RAW_OUTPUT_DIR = "data_generation/outputs/impossible_generations/raw/"
 CORRECTED_OUTPUT_DIR = "data_generation/outputs/impossible_generations/corrected/"
 
@@ -21,6 +22,10 @@ def preprocess_token_tensor(tensor, eos_token_id) -> list:
         output = output[:-1] # Remove the EOS token if it's there
     return output
 
+def write_dict_to_file(dict, filepath):
+    with open(filepath, "w+") as f:
+        json.dump(dict, f, indent=4, ensure_ascii=False)
+
 for perturbation in VALID_UNDO_PERTURBATION_KEYS:
     model_id = PERTURBATION_TO_HF_MODEL_NAME[perturbation]
     model = GPT2LMHeadModel.from_pretrained(model_id).to(device)
@@ -31,13 +36,12 @@ for perturbation in VALID_UNDO_PERTURBATION_KEYS:
     input_ids = torch.tensor([[tokenizer.bos_token_id]]).to(device)
     attention_mask = torch.ones_like(input_ids).to(device)
 
-    raw_outputs = []
-    corrected_outputs = []
+    raw_outputs = {}
+    corrected_outputs = {}
 
     for i in tqdm(range(NUM_LINES), desc=f"Generating for {perturbation}"):
         output = model.generate(input_ids, attention_mask=attention_mask, max_new_tokens=50, pad_token_id=tokenizer.eos_token_id, do_sample=True)
         generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-        # raw_outputs.append(output[0])
 
         undo_input = preprocess_token_tensor(output, tokenizer.eos_token_id)
 
@@ -49,19 +53,13 @@ for perturbation in VALID_UNDO_PERTURBATION_KEYS:
         
         corrected_text = UNDO_PERTURBATIONS[perturbation]['gpt2_tokenizer'].decode(corrected_output, skip_special_tokens=True)
 
-        raw_outputs.append(undo_input)
-        corrected_outputs.append(corrected_output)
-        # corrected_outputs.append(corrected_text)
+        raw_outputs[i] = generated_text
+        corrected_outputs[i] = corrected_text
 
     # Save generated text to file
-    with open(RAW_OUTPUT_DIR + f"{perturbation}.txt", "w+") as f:
-        for line in raw_outputs:
-            f.write(f"{line}\n")
-    
-    with open(CORRECTED_OUTPUT_DIR + f"{perturbation}.txt", "w+") as f:
-        for line in corrected_outputs:
-            f.write(f"{line}\n")
-    
+    write_dict_to_file(raw_outputs, RAW_OUTPUT_DIR + f"{perturbation}.txt")
+    write_dict_to_file(corrected_outputs, CORRECTED_OUTPUT_DIR + f"{perturbation}.txt")
+
     print(f"Generated {NUM_LINES} sentences for {perturbation}.")
 
 for perturbation in ['english', 'shuffle_nondeterministic']:
@@ -74,16 +72,38 @@ for perturbation in ['english', 'shuffle_nondeterministic']:
     input_ids = torch.tensor([[tokenizer.bos_token_id]]).to(device)
     attention_mask = torch.ones_like(input_ids).to(device)
 
-    raw_outputs = []
+    raw_outputs = {}
 
     for i in tqdm(range(NUM_LINES), desc=f"Generating for {perturbation}"):
         output = model.generate(input_ids, attention_mask=attention_mask, max_new_tokens=50, pad_token_id=tokenizer.eos_token_id, do_sample=True)
-        # generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-        raw_outputs.append(preprocess_token_tensor(output, tokenizer.eos_token_id))
+        generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+        raw_outputs[i] = generated_text
 
     # Save generated text to file
-    with open(CORRECTED_OUTPUT_DIR + f"{perturbation}.txt", "w+") as f:
-        for line in raw_outputs:
-            f.write(f"{line}\n")
-    
+    write_dict_to_file(raw_outputs, CORRECTED_OUTPUT_DIR + f"{perturbation}.txt")
+
     print(f"Generated {NUM_LINES} sentences for {perturbation}.")
+
+external_models = ["openai-community/gpt2"]
+for model_id in external_models:
+    model = GPT2LMHeadModel.from_pretrained(model_id).to(device)
+    model.eval()
+    tokenizer = GPT2Tokenizer.from_pretrained(model_id)
+
+    # Start with just the BOS token (no initial prompt)
+    input_ids = torch.tensor([[tokenizer.bos_token_id]]).to(device)
+    attention_mask = torch.ones_like(input_ids).to(device)
+
+    raw_outputs = {}
+
+    for i in tqdm(range(NUM_LINES), desc=f"Generating for {model_id}"):
+        output = model.generate(input_ids, attention_mask=attention_mask, max_new_tokens=50, pad_token_id=tokenizer.eos_token_id, do_sample=True)
+        generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+        raw_outputs[i] = generated_text
+
+    model_name = model_id.replace("/", "_")
+    
+    # Save generated text to file
+    write_dict_to_file(raw_outputs, CORRECTED_OUTPUT_DIR + f"{model_name}.txt")
+    
+    print(f"Generated {NUM_LINES} sentences for {model_name}.")
